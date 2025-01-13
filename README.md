867d50d33a013145773e59399b877bac6d45d9f0
longzai <longzai.com>
2025-01-13 17:31:01 +0800

    feat: 增加vue模板

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # imc-devcie-prd-web-util
 
-使用monorepo架构，存放eslint、stylelint等配置，存放设备团队前端脚手架项目，以及后续开发的一些工具
+使用monorepo架构，存放eslint、stylelint等配置，存放前端脚手架项目，以及后续开发的一些工具
---
+++ packages/vue-template/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useMainStore } from '@/store'
+
+const mainStore = useMainStore()
+
+const { count, doubleCount } = storeToRefs(mainStore)
+const { increment } = mainStore
+</script>
+
+<template>
+  <div>
+    <div @click="increment">点击</div>
+    <div>次数：{{ count }}</div>
+    <div>双倍次数：{{ doubleCount }}</div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
